64a536d8e1d00bce3905f3ac1d3ca4ca4bba2356
Patrick Segarel <contact@patrick-segarel.com>
2014-12-11 22:44:08 +0800

    Adds pretty urls, .htaccess, also initiates routes with page not found template

--- js/startup.js
+++ js/startup.js
@@ -4,5 +4,30 @@
 		[	'ngRoute',
 			'ui.bootstrap'
 	]);
+	
+	app.config( function($routeProvider , $locationProvider ){
+		
+		$routeProvider
+			.when('/' , {
+				templateUrl: 'views/index.html'
+			})
+			.otherwise({
+				templateUrl: 'views/pagenotfound.html',
+				controller: 'UtilsController'
+			});
+
+		// use the HTML5 History API
+		$locationProvider.html5Mode(true);
+	});
+
+	app.controller('UtilsController' , function($scope, $route, $routeParams, $location){
+		
+		$scope.route = $route;
+		$scope.routeParams = $routeParams;
+		$scope.location = $location;
+		$scope.home = function(){
+			$location.path('/');
+		};
+	});
 }
 )();
