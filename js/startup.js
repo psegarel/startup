028d578e9dcdf5d98db20ccb607706b814d5ed08
Patrick Segarel <contact@patrick-segarel.com>
2014-12-27 21:09:24 +0800

    Switch to ui-router

--- js/startup.js
+++ js/startup.js
@@ -1,35 +1,11 @@
 ( function()
 {
-	var app = angular.module('startup' , 
-		[	'ngRoute',
-			'ui.bootstrap',
-			// Uncommment for images-loaded suppport, don't forget to uncomment the scripts!
-			//'angular-images-loaded'
+	var app = angular.module('startup' , [
+		'ui.router'
 	]);
 	
-	app.config( function($routeProvider , $locationProvider ){
-		
-		$routeProvider
-			.when('/' , {
-				templateUrl: 'views/index.html'
-			})
-			.otherwise({
-				templateUrl: 'views/pagenotfound.html',
-				controller: 'UtilsController'
-			});
+	app.controller('UtilsController' , function($scope){
 
-		// use the HTML5 History API
-		$locationProvider.html5Mode(true);
-	});
-
-	app.controller('UtilsController' , function($scope, $route, $routeParams, $location){
-		
-		$scope.route = $route;
-		$scope.routeParams = $routeParams;
-		$scope.location = $location;
-		$scope.home = function(){
-			$location.path('/');
-		};
 	});
 }
 )();
